c372ffa0dc4fc0432eabc66cd0856d83e3adce60
mikk922r <mikk922r@edu.mercantec.dk>
2025-05-05 11:24:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿namespace Banko
+{
+    public class Program
+    {
+        public List<Plate> plates = new List<Plate>();
+
+        public List<int> guessedNumbers = new List<int>();
+
+        public bool gameOver = false;
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.Run();
+        }
+
+        private void Run()
+        {
+            while (plates.Count == 0)
+            {
+                Console.WriteLine("Indtast plade-id'er (komma-separeret):");
+
+                string[] plateIds = (Console.ReadLine()?.Split(",") ?? []).Select(x => x.Trim()).ToArray();
+
+                Scraper scraper = new Scraper();
+
+                plates.AddRange(scraper.GetPlates(plateIds));
+            }
+
+            while (plates.Count > 0 && !gameOver)
+            {
+                ShowPlates();
+
+                string input = Console.ReadLine();
+
+                GuessNumber(input);
+            }
+        }
+
+        private void ShowPlates()
+        {
+            Console.Clear();
+
+            foreach (var plate in plates)
+            {
+                Console.WriteLine($"Plade: {plate.Id}");
+
+                for (int i = 0; i < plate.Numbers.Count; i++)
+                {
+                    Console.Write($"{i + 1}: ");
+
+                    foreach (var value in plate.Numbers[i])
+                    {
+                        if (guessedNumbers.Contains(value))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ResetColor();
+                        }
+
+                        Console.Write($"{value} ");
+                    }
+
+                    Console.ResetColor();
+
+                    if (plate.Numbers[i].All(x => guessedNumbers.Contains(x)))
+                    {
+                        Console.Write("- Banko!");
+                    }
+
+                    Console.Write("\n");
+                }
+
+                Console.Write("\n");
+            }
+        }
+
+        private void GuessNumber(string input)
+        {
+            if (!int.TryParse(input, out int guessedNumber))
+            {
+                return;
+            }
+
+            guessedNumbers.Add(guessedNumber);
+
+            foreach (var plate in plates)
+            {
+                foreach (var numbers in plate.Numbers)
+                {
+                    if (!numbers.Contains(guessedNumber))
+                    {
+                        continue;
+                    }
+
+                    if (!numbers.All(x => guessedNumbers.Contains(x)))
+                    {
+                        continue;
+                    }
+
+                    gameOver = true;
+                }
+            }
+
+            ShowPlates();
+
+            Plate? winningPlate = plates.FirstOrDefault(plate => plate.Numbers.Any(row => row.All(n => guessedNumbers.Contains(n))));
+
+            if (winningPlate is null)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Banko på pladen: {winningPlate.Id}");
+        }
+    }
+
+    public class Plate
+    {
+        public string Id { get; set; }
+
+        public List<List<int>> Numbers { get; set; }
+    }
+}
---
+++ Scraper.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Banko
+{
+    public class Scraper
+    {
+        public List<Plate> GetPlates(string[] plateIds)
+        {
+            List<Plate> plates = new List<Plate>();
+
+            IWebDriver driver = GetWebDriver();
+
+            driver.Navigate().GoToUrl("https://mercantech.github.io/Banko/");
+
+            foreach (string plateId in plateIds)
+            {
+                Plate plate = new Plate()
+                {
+                    Id = plateId,
+                    Numbers = new List<List<int>>()
+                };
+
+                IWebElement input = driver.FindElement(By.Id("tekstboks"));
+
+                input.Clear();
+
+                input.SendKeys(plateId);
+
+                IWebElement button = driver.FindElement(By.Id("knap"));
+
+                button.Click();
+
+                IReadOnlyCollection<IWebElement> plateElements = driver.FindElements(By.TagName("tr"));
+
+                foreach (IWebElement plateElement in plateElements)
+                {
+                    IReadOnlyCollection<IWebElement> plateNumbers = plateElement.FindElements(By.TagName("td"));
+
+                    List<int> numbers = plateNumbers.Where(x => !string.IsNullOrEmpty(x.Text)).Select(x => Convert.ToInt32(x.Text)).ToList();
+
+                    plate.Numbers.Add(numbers);
+                }
+
+                plates.Add(plate);
+            }
+
+            driver.Quit();
+
+            return plates;
+        }
+
+        private IWebDriver GetWebDriver()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+
+            service.EnableVerboseLogging = false;
+            service.SuppressInitialDiagnosticInformation = true;
+            service.HideCommandPromptWindow = true;
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.AddArgument("--headless");
+
+            return new ChromeDriver(service, options);
+        }
+    }
+}
